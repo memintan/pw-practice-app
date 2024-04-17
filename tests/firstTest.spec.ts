6668e364edba719766347016c0565bf55215dd57
memintan <memintan@gmail.com>
2024-04-16 21:47:00 -0500

    thats ok

---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,39 @@
+import {test} from '@playwright/test'
+import { __await } from 'tslib'
+
+test.beforeEach(async({page}) => {
+    await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+})
+
+test('Locator syntax rules',async({page}) => {
+    //by Tag name
+    page.locator('input')
+
+    //by ID
+    page.locator('#inputEmail1')
+
+    //by Class value
+    page.locator('.shape-rectangle')
+
+    //by Attribute
+    page.locator('[placeholder="Email"]')
+
+    //by Class value(full)
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition cdk-focused cdk-mouse-focused"]')
+
+    // combine different selectors
+    page.locator('input[placeholder="Email"][nbinput]')
+
+    //by xpath(not recommonded)
+    page.locator('//*[@id="inputEmail1"]')
+
+    //by partial text match
+    page.locator(':text("Using")')
+
+    //by partial text match
+    page.locator(':text-is("Using the Grid")')
+  
+})
+
