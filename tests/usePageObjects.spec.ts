17c3776fd12e656aa66385633287b36f06d9247d
memintan <memintan@gmail.com>
2024-04-19 00:40:25 -0500

    inline form filling arranged

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import {test, expect} from '@playwright/test'
 import{NavigationPage} from '../page-objects/navigationPage'
+import { FormLayoutsPage } from '../page-objects/formLayoutsPage'
 
 test.beforeEach(async({page}) => {
     await page.goto('http://localhost:4200/')
@@ -12,4 +13,13 @@ test('Navigate to Form Page', async({page}) => {
     await navigateTo.smartTablePage()
     await navigateTo.toastrPage()
     await navigateTo.toolTipPage()
+})
+
+test('Parametrized Methods', async({page}) =>{
+    const navigateTo = new NavigationPage(page)
+    const onFormLayoutPage = new FormLayoutsPage(page)
+
+    await navigateTo.formLayoutsPage()
+    await onFormLayoutPage.submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 1')
+    await onFormLayoutPage.submitInlineFormWithNameAndCheckbox('John Smith', "john@test.com", true)
 })
